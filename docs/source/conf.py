a2caa6c15e9c1f8c57b993c3e4a760e57901b4e4
Mark Sevelj <mark.sevelj@dunwright.com.au>
2021-08-07 08:57:11 +0000

    Added autoclasstoc_2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,7 @@ extensions = [  "sphinx.ext.autodoc",
                 "sphinx.ext.autosummary",
                 "sphinx_copybutton",
                 "sphinx_inline_tabs",
+                 "autoclasstoc_2",
             ]
 
 # Add any paths that contain templates here, relative to this directory.
