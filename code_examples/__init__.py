3985e6e58c8dee9366a8ea202f7cb17bd936af92
Mark Sevelj <mark.sevelj@dunwright.com.au>
2021-08-07 06:20:31 +0000

    Initial Commit

---
+++ code_examples/__init__.py
@@ -0,0 +1,7 @@
+"""Code Examples init docstring"""
+
+# from . import reST_dummy
+
+# from . import square_example
+
+# __all__ = ["reST_dummy", "square_example"]
---
+++ code_examples/simple_calculators_example.py
@@ -0,0 +1,26 @@
+""" A very very simple set of calculators"""
+
+
+def square(a):
+    """a very simple squaring function
+
+    long description: returns the square of a: :math:`a^2`
+
+    :param a: an input argument
+
+    :returns: a*a
+    """
+    return a * a
+
+
+def multiply(a, b):
+    """a very simple multiplying function
+
+    long description: returns the square of b: :math:`a*b`
+
+    :param a: an input argument
+    :param b: an input argument
+
+    :returns: a*b
+    """
+    return a * b
