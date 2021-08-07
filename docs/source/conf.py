e180049950d26773346f69143982da7e08aa590f
Mark Sevelj <mark.sevelj@dunwright.com.au>
2021-08-07 06:46:58 +0000

    Changed to pygments monakai

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,6 +49,8 @@ exclude_patterns = []
 #
 html_theme = "alabaster"
 html_theme = "furo"
+pygments_style = "monokai"
+pygments_dark_style = "monokai"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
